d67ba2f6c9deba2d6f1acced650deb9312836ef7
Damian Lattenero <damianlattenero@gmail.com>
2016-11-04 11:47:21 -0300

    funciona colocar celdas vivas y figuras

--- src/edu/unq/pconc/gameoflife/solution/GameOfLifeGrid.java
+++ src/edu/unq/pconc/gameoflife/solution/GameOfLifeGrid.java
@@ -1,6 +1,7 @@
 package edu.unq.pconc.gameoflife.solution;
 
 import edu.unq.pconc.gameoflife.CellGrid;
+import javafx.scene.control.Cell;
 
 import java.awt.*;
 
@@ -8,17 +9,24 @@ import java.awt.*;
  * Created by coord-inf on 04/11/16.
  */
 public class GameOfLifeGrid implements CellGrid{
-    private Dimension dimension = new Dimension(50,50);
+    private Dimension dimension;
+    private boolean grid[][];
 
+//    private Dimension dimension = new Dimension(50,50);
+
+    public GameOfLifeGrid() {
+        this.dimension = new Dimension();
+        this.grid = new boolean[0][0];
+    }
 
     @Override
     public boolean getCell(int col, int row) {
-        return false;
+        return grid[col][row];
     }
 
     @Override
     public void setCell(int col, int row, boolean cell) {
-
+        grid[col][row] = cell;
     }
 
     @Override
@@ -28,7 +36,8 @@ public class GameOfLifeGrid implements CellGrid{
 
     @Override
     public void resize(int col, int row) {
-
+        this.dimension = new Dimension(col, row);
+        this.grid = new boolean[col][row];
     }
 
     @Override
